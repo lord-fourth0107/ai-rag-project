638e9e56c89eb5d8dc2e672c8742a335a1125024
lord-fourth0107 <shivamsinghnitr123@gmail.com>
2025-02-21 00:22:42 -0500

    changes

--- settings.py
+++ settings.py
@@ -3,5 +3,4 @@ OLLAMA_HOST_URL = "http://ollama_container:11434"
 MONGO_HOST_URL = "mongodb://mongodb:27017/"
 QDRANT_HOST_URL = "http://qdrant:6333"
 MODEL_ID='llama3:latest'
-#'hf.co/nsh22/ROS-gguf'
 RUN_MODE = "APP"
